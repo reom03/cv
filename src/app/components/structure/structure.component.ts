7f7c4749ab76f6c9ff1a85e112ec65f4c0ab604e
reom03 <reom03@gmil.com>
2024-10-02 21:47:26 -0300

    fix cicd

---
+++ src/app/components/structure/structure.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit } from '@angular/core';
+import {ArrayDataSource} from '@angular/cdk/collections';
+import {FlatTreeControl} from '@angular/cdk/tree';
+import {FilesService} from '../../services/files.service';
+
+@Component({
+  selector: 'app-structure',
+  templateUrl: './structure.component.html',
+  styleUrls: ['./structure.component.css']
+})
+export class StructureComponent implements OnInit {
+
+  TREE_DATA: ProjectNode[] = [
+    {
+      name: 'Raul Ortiz',
+      expandable: true,
+      className: '',
+      isExpanded: true,
+      level: 0,
+    },
+    {
+      name: 'About me',
+      expandable: false,
+      level: 1,
+      className: 'fas fa-user-circle color-pink',
+    },
+    {
+      name: 'Education',
+      expandable: false,
+      level: 1,
+      className: 'fas fa-graduation-cap color-pink',
+    }, {
+      name: 'Work Experience',
+      expandable: false,
+      level: 1,
+      className: 'fas fa-id-card-alt color-pink',
+    }, {
+      name: 'Skills',
+      expandable: false,
+      className: 'fas fa-code color-pink',
+      level: 1
+    }, {
+      name: 'Languages',
+      expandable: false,
+      level: 1,
+      className: 'fas fa-language color-pink',
+    }
+  ];
+  treeControl = new FlatTreeControl<ProjectNode>(
+    node => node.level, node => node.expandable);
+
+  dataSource = new ArrayDataSource(this.TREE_DATA);
+
+  constructor(private filesService: FilesService) { }
+
+  ngOnInit(): void {
+  }
+  hasChild = (_: number, node: ProjectNode) => node.expandable;
+
+  getParentNode(node: ProjectNode): any{
+    const nodeIndex = this.TREE_DATA.indexOf(node);
+
+    for (let i = nodeIndex - 1; i >= 0; i--) {
+      if (this.TREE_DATA[i].level === node.level - 1) {
+        return this.TREE_DATA[i];
+      }
+    }
+
+    return null;
+  }
+
+  shouldRender(node: ProjectNode): boolean{
+    const parent = this.getParentNode(node);
+    return !parent || parent.isExpanded;
+  }
+
+  selectTab(tab: string): void {
+    this.filesService.openTab(tab);
+  }
+}
+/** Flat node with expandable and level information */
+interface ProjectNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+  className: string;
+  isExpanded?: boolean;
+}
---
+++ src/app/components/topbar/topbar.component.ts
@@ -0,0 +1,86 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
+import introJs from 'intro.js';
+
+@Component({
+  selector: 'app-topbar',
+  templateUrl: './topbar.component.html',
+  styleUrls: ['./topbar.component.css']
+})
+export class TopbarComponent implements OnInit {
+
+  introJS = introJs();
+  constructor(public dialog: MatDialog) {
+    this.introJS.setOptions({
+      steps: [
+        {
+          intro: 'Welcome to my IDE Theme Resume!',
+          position: 'bottom'
+        },
+        {
+          element: '#me',
+          intro: 'That would be me, and what I consider my self professionally or what I know the most',
+          position: 'bottom'
+        },
+        {
+          element: '#menu',
+          intro: 'Here you can download my CV in PDF, or take a look at my social media!!',
+          position: 'top'
+        },
+        {
+          element: '#files',
+          intro: 'This you can find all my information, just click on each \"File\" and it will open as a tab',
+          position: 'right'
+        },
+        {
+          element: '#tabs',
+          intro: 'The info will show up here, you can close and open again as you want',
+          position: 'top'
+        },
+        {
+          element: '#shell',
+          intro: 'Here is a Shell for you to enjoy, give it a try! It was pretty fun to develop this part. You can start by typing help and hitting enter.',
+          position: 'right'
+        },
+        {
+          element: '#center',
+          intro: 'Well that\'s all, knock yourself out, and thanks for the visit!!!',
+          position: 'right'
+        }
+      ],
+      showProgress: true,
+    });
+
+  }
+
+  ngOnInit(): void {
+
+    if (!localStorage.getItem('dismiss')) {
+      this.introJS.start();
+    }
+    this.introJS.onexit(() => {
+      localStorage.setItem('dismiss', 'true');
+    });
+  }
+
+  openSnackBar(): void {
+    const dialogRef = this.dialog.open(AboutDialogComponent);
+  }
+
+}
+
+@Component({
+  selector: 'app-about',
+  templateUrl: 'about-dialog.html',
+})
+export class AboutDialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<AboutDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
